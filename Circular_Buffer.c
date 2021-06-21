81f4820f0dda1795c6aaadf936c75bdcd168c785
Meed97 <meedo713@gmail.com>
2021-06-22 04:31:25 +0800

    Updated code

--- Circular_Buffer.c
+++ Circular_Buffer.c
@@ -16,3 +16,31 @@ osSemaphoreId item_semaphore;                         // Semaphore ID
 osSemaphoreDef(item_semaphore);                       // Semaphore definition
 osSemaphoreId space_semaphore;                         // Semaphore ID
 osSemaphoreDef(space_semaphore);                       // Semaphore definition
+
+long int p=0;
+long int c=0;
+
+const unsigned int SIZE = 4;
+unsigned char buffer[SIZE];
+unsigned int ins_Index = 0;
+unsigned int rem_Index = 0;
+
+void put(unsigned char an_item){
+	osSemaphoreWait(space_semaphore, osWaitForever);
+	osMutexWait(x_mutex, osWaitForever);
+	buffer[ins_Index] = an_item;
+	ins_Index = (ins_Index + 1) % SIZE;
+	osMutexRelease(x_mutex);
+	osSemaphoreRelease(item_semaphore);
+}
+
+unsigned char get(){
+	unsigned int out_data = 0xff;
+	osSemaphoreWait(item_semaphore, osWaitForever);
+	osMutexWait(x_mutex, osWaitForever);
+	out_data = buffer[rem_Index];
+	rem_Index = (rem_Index + 1) % SIZE;
+	osMutexRelease(x_mutex);
+	osSemaphoreRelease(space_semaphore);
+	return out_data;
+}
